08e619a79d0e49fdc6139df31da475ef3d8ccb95
WhirlyByrd <kara.r.gardner@gmail.com>
2022-09-21 16:02:56 -0500

    fixed error

--- server/index.js
+++ server/index.js
@@ -9,13 +9,19 @@ const app = express()
 app.use(express.json())
 
 //middleware here
-app.use('/', express.static(path.join(__dirname, '../client/index.html')))
-app.use(express.static(path.join(__dirname, '../client')))
+// app.use('/', express.static(path.join(__dirname, '../client/index.html')))
+// app.use(express.static(path.join(__dirname, '../client')))
 
 //////////////////////////////////////////////////////////////////
 
 
+app.get('/',(req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+} )
 
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/main.js'))
+} )
 
 
 
