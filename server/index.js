1776759b13549fd7efb1c2ef078a748e4a0d1e63
WhirlyByrd <kara.r.gardner@gmail.com>
2022-09-21 15:26:41 -0500

    fixed h1

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 require('dotenv').config()
 
-const port = process.env.PORT || 4500
+const port = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json())
